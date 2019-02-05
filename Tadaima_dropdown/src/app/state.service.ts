a4cbf19493d9ee942ac3bf9ed2a4353f69fbb906
carlos Llorente <llorenteyuste@hotmail.com>
2019-02-05 21:40:18 +0100

    Clean

--- Tadaima_dropdown/src/app/state.service.ts
+++ Tadaima_dropdown/src/app/state.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
-import { catchError, map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
 
 import { States } from './states';
 
